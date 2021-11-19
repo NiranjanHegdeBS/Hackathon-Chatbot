224be77c17306b7bb27f6cbfa848a7110b79c4e6
NiranjanHegdeBS <nbs@dovercorp.com>
2021-11-19 09:06:22 +0530

    Use Case 1 Updated

--- app.py
+++ app.py
@@ -2,31 +2,34 @@ from flask import Flask, request, jsonify
 import json
 app = Flask(__name__)
 
+chatbot_qa = json.loads(open('chatbot.json').read())
+
 @app.route("/")
 def home():
     return "Welcome to Customer Support Chatbot"
 
-start = 1 
-chatbot_qa = json.loads(open('chatbot.json').read())
-
 @app.route("/api/messages",methods=["POST","GET"])
 def chatbot_response():
-    global start
-    if(start):
+    response_msg = {}
+
+    if request.method == "GET":
         question = chatbot_qa[0]["que"]
         follow_up = chatbot_qa[0]["options"]
         response_msg = {"question":question,"follow_up":follow_up}
-        start = 0
-        return jsonify(response_msg)
-    else:
-        print(request.get_json())
-        return(jsonify(request.get_json()))
-    # msg = request.form["msg"]
-    # if(sentiment_analyser(msg) == "POSITIVE"):
-    #     response_msg = {}
         
-
-
-
+    elif request.method == "POST":
+        user_option_info = request.get_json()
+        key = -1
+        for i in range(len(chatbot_qa)):
+            if int(chatbot_qa[i]["id"]) == user_option_info["key"]:
+                key = i
+                break
+              
+        question = chatbot_qa[key]["que"]
+        follow_up = chatbot_qa[key]["options"]
+        response_msg = {"question":question,"follow_up":follow_up}
+    
+    return(jsonify(response_msg))
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0")
